246a2736160bb5052f7e2f9cf5ef8ab007312861
GuillermoVTec <117295197+GuillermoVTec@users.noreply.github.com>
2023-07-24 17:27:01 -0500

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-   <title>{{'CRM Preference Passport'}}</title>
+   <title>{{'CRM Preference Passport2'}}</title>
 
     <meta name="description" content="" />
     <!-- Favicon -->

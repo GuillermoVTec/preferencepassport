5632126fcd8a4091720122a7f5be85467c5d795e
GuillermoVTec <117295197+GuillermoVTec@users.noreply.github.com>
2023-07-27 11:56:53 -0500

    Update ProcessLeads.php

--- app/Jobs/ProcessLeads.php
+++ app/Jobs/ProcessLeads.php
@@ -63,7 +63,7 @@ class ProcessLeads implements ShouldQueue
             'url' => 'https://preferencepassport.com/preferencepassport/login' 
         ];
             //var_dump($mailData);die();
-            Mail::to($lead->correo)->send(new correo($mailData->Email));
+            Mail::to($lead->correo)->send(new correo('o'));
             
         
       

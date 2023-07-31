e0990212ec6d12d6e9c80b51cd0830c6f6cf6260
GuillermoVTec <117295197+GuillermoVTec@users.noreply.github.com>
2023-07-31 11:03:43 -0500

    ok

--- resources/views/lead/create.blade.php
+++ resources/views/lead/create.blade.php
@@ -5,27 +5,21 @@
 @endsection
 
 @section('content')
-    <section >
-       
-            <div class="col-md-12">                        
-                                
+    <section>
+            <div class="col-md-12">                                  
                                  <h4 class="fw-bold py-3 mb-4"> <i class="bx bx-credit-card me-1"></i> Registrar Tarjeta</h4>
                 @includeif('partials.errors')
-
                 <div class="card card-default">
-                
                     <div class="card-header">
-                      <h5 class=""><small class="text-muted float-end"><a class="btn btn-info"  href="{{ route('home') }}">Regresar</a></small></h5>
+                      <h5 class=""><small class="text-muted float-end"><a class="btn btn-info"  href="{{route('home')}}">Regresar</a></small></h5>
                     </div>
-
                     <div class="card-body">
-                        <form method="POST" action="{{ route('leads.store') }}"  role="form" enctype="multipart/form-data">
+                        <form method="POST" action="{{route('leads.store')}}"  role="form" enctype="multipart/form-data">
                             @csrf
-
                             @include('lead.form')
                             <div class="fw-bold py-3 mb-4">
                                 <button type="submit" class="btn btn-info">Guardar</button>
-                                <a type="reset" href="{{ route('home') }}" class="btn btn-outline-secondary">Cancelar </a>
+                                <a type="reset" href="{{route('home')}}" class="btn btn-outline-secondary">Cancelar </a>
                              </div>
                         </form>
                      </div>
--- storage/framework/views/0ac28fd491d039ab5f1ae22cdc675ed99cb52311.php
+++ storage/framework/views/0ac28fd491d039ab5f1ae22cdc675ed99cb52311.php
@@ -5,23 +5,17 @@
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('content'); ?>
-    <section >
-       
-            <div class="col-md-12">                        
-                                
+    <section>
+            <div class="col-md-12">                                  
                                  <h4 class="fw-bold py-3 mb-4"> <i class="bx bx-credit-card me-1"></i> Registrar Tarjeta</h4>
                 <?php if ($__env->exists('partials.errors')) echo $__env->make('partials.errors', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-
                 <div class="card card-default">
-                
                     <div class="card-header">
                       <h5 class=""><small class="text-muted float-end"><a class="btn btn-info"  href="<?php echo e(route('home')); ?>">Regresar</a></small></h5>
                     </div>
-
                     <div class="card-body">
                         <form method="POST" action="<?php echo e(route('leads.store')); ?>"  role="form" enctype="multipart/form-data">
                             <?php echo csrf_field(); ?>
-
                             <?php echo $__env->make('lead.form', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
                             <div class="fw-bold py-3 mb-4">
                                 <button type="submit" class="btn btn-info">Guardar</button>
